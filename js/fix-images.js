5eb6c1b287e6864178384ecd90ddc32bb24e5cce
Stephen MacNeil <smacneil01@gmail.com>
2015-10-31 12:55:57 -0400

    relative link for error image

--- js/fix-images.js
+++ js/fix-images.js
@@ -1,6 +1,6 @@
 //Needs JQuery
 
 $('.thumbnail img').error(function(){
-  $(this).attr('src', 'http://localhost:4000/images/question.png');
+  $(this).attr('src', '/images/question.png');
   $(this).parent().parent().attr('style', 'opacity:0;')
 });
