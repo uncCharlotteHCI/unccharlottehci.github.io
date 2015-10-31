1b23628139b00c86f12932cdb91dd52ed726c6db
Stephen MacNeil <smacneil01@gmail.com>
2015-10-31 13:18:13 -0400

    keep this just for good measure / extra security

--- js/fix-images.js
+++ js/fix-images.js
@@ -1,6 +1,9 @@
 //Needs JQuery
 
 $('.thumbnail img').error(function(){
-  //$(this).attr('src', '/images/question.png');
   $(this).parent().parent().attr('style', 'opacity:0;')
+});
+
+$('img').error(function(){
+  $(this).attr('style', 'opacity:0;')
 });
