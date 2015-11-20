d9284fff1fdce2178f2ee142d30c83a50634c103
Stephen MacNeil <smacneil01@gmail.com>
2015-11-20 13:18:39 -0500

    remove site files, include build file

--- _site/js/fix-images.js
+++
@@ -1,9 +0,0 @@
-//Needs JQuery
-
-$('.thumbnail img').error(function(){
-  $(this).parent().parent().attr('style', 'opacity:0;')
-});
-
-$('img').error(function(){
-  $(this).attr('style', 'display: none;')
-});
